622daf60a9bd8d883d3e5c9a61b86890bd8cbc68
Dan Walker <dwalker@rm.com>
2021-06-10 12:30:48 +0100

    Added translation flag

--- fortigate2csv.py
+++ fortigate2csv.py
@@ -2,7 +2,7 @@
 
 # author    dan walker <code@danwalker.com>
 # created   2020-11-12
-# updated   2020-11-23
+# updated   2021-06-03
 # url       github.com/danwalkeruk/fortigate2csv
 
 import argparse
@@ -10,6 +10,7 @@ import getpass
 import sys
 import requests
 import json
+from netaddr import IPAddress
 
 # disable warnings for insecure connections
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -21,14 +22,18 @@ item_types = ['interface', 'policy', 'snat', 'address', 'service', 'dnat', 'pool
 def main():
     # build a parser, set arguments, parse the input
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--firewall', help='Firewall Host', required=True)
-    parser.add_argument('-u', '--user', help='Username', required=True)
-    parser.add_argument('-v', '--vdom', help='VDOM', required=True)
-    parser.add_argument('-i', '--item', help='Item', required=True)
-    parser.add_argument('-o', '--outfile', help='Output CSV file', required=False)
+    parser.add_argument('-f', '--firewall',  help='Firewall', required=True)
+    parser.add_argument('-u', '--user',      help='Username', required=True)
+    parser.add_argument('-v', '--vdom',      help='VDOM', required=True)
+    parser.add_argument('-i', '--item',      help='Item', required=True)
+    parser.add_argument('-t', '--translate', help='Include translation of IP objects', action='store_true')
+    parser.add_argument('-o', '--outfile',   help='Output file')
     args = parser.parse_args()
 
     base_url = f'https://{args.firewall}/'
+
+    # holds an address lookup table, if requested
+    addresses = {}
 
     # check item type
     if args.item not in item_types:
@@ -41,6 +46,32 @@ def main():
     # connect and authenticate
     print(f'Connecting to {args.firewall} ({args.vdom}) as {args.user}')
     f = f_login(args.firewall, args.user, password, args.vdom)
+
+    # build a translation table if required
+    if args.translate:
+        print('Building lookup tables')
+        # add addresses to the local lookup table
+        data = f.get(f'{base_url}api/v2/cmdb/firewall/address/?vdom={args.vdom}').json()
+        for address in data['results']:
+            if address['type'] == 'ipmask':
+                ip, subnet = address['subnet'].split()
+                addresses[address['name']] = f'{ip}/{IPAddress(subnet).netmask_bits()}'
+            elif address['type'] == 'iprange':
+                addresses[address['name']] = f'{address["start-ip"]}-{address["end-ip"]}'
+
+        # add ip pools to the local lookup table
+        data = f.get(f'{base_url}api/v2/cmdb/firewall/ippool/?vdom={args.vdom}').json()
+        for address in data['results']:
+            if address['startip'] == address['endip']:
+                addresses[address['name']] = address['startip']
+            else:
+                addresses[address['name']] = f"{address['startip']}-{address['endip']}"
+
+        # add vips to the local lookup table
+        data = f.get(f'{base_url}api/v2/cmdb/firewall/vip/?vdom={args.vdom}').json()
+        for address in data['results']:
+            addresses[address['name']] = address['extip']
+
     print('Fetching data...')
 
     # DNAT and VIPs
@@ -96,7 +127,10 @@ def main():
         print('Firewall returned no results')
         sys.exit(1)
     
-    csv_data = build_csv(headers, data['results'])
+    if args.translate:
+        csv_data = build_csv(headers, data['results'], addresses)
+    else:
+        csv_data = build_csv(headers, data['results'])
 
     # display or save
     if not args.outfile:
@@ -111,7 +145,7 @@ def main():
 
     print(f'Done!')
 
-def build_csv(headers, rows):
+def build_csv(headers, rows, address_lookup=None):
     """ 
     Return a formatted CSV, dynamically generated from headers/data
 
@@ -123,11 +157,13 @@ def build_csv(headers, rows):
     # create csv output variable + add headers
     csv = f"{','.join(map(str, headers))}\n"
 
+    # ROWS
     for row in rows:
         row_data = [] # holds the data for each row before we add to csv
 
-        # we only want to extract fields that match our headers
+        # COLUMNS
         for header in headers:
+            # we only want to extract fields that match our headers
             if header in row:
                 # some fields are lists of dicts
                 if type(row[header]) == list:
@@ -141,7 +177,10 @@ def build_csv(headers, rows):
                                 subitems.append(f"{x['ip']}/{x['cidr_netmask']}")
                         else: # parse list, extract q_origin_key for each item within the field
                             for x in row[header]:
-                                subitems.append(x['q_origin_key'])
+                                if address_lookup and x['q_origin_key'] in address_lookup:
+                                    subitems.append(address_lookup[x['q_origin_key']]) 
+                                else:
+                                    subitems.append(x['q_origin_key']) 
                         # join with a space, can't use comma due to csv
                         row_data.append(' '.join(map(str, subitems))) 
                 else:
@@ -149,7 +188,10 @@ def build_csv(headers, rows):
                     if type(row[header]) == str:
                         row_data.append(row[header].replace(",", ""))
                     else:
-                        row_data.append(row[header])
+                        if address_lookup and row[header] in address_lookup:
+                            subitems.append(address_lookup[row[header]]) 
+                        else:
+                            row_data.append(row[header])
             else:
                 # display blanks where we have no info for this header
                 row_data.append('')
@@ -157,6 +199,8 @@ def build_csv(headers, rows):
         csv += f"{','.join(map(str, row_data))}\n"
 
     return csv
+
+
 
 
 def f_login(host,user,password,vdom):
